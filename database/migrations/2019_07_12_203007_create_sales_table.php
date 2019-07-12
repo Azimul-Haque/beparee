092ca6acbf43b5138984c7608fcd3937d24adfcc
orbachinujbuk <orbachinujbuk@gmail.com>
2019-07-13 01:52:01 +0600

    Sales Migrated

--- database/migrations/2019_07_12_203007_create_sales_table.php
+++ database/migrations/2019_07_12_203007_create_sales_table.php
@@ -15,6 +15,17 @@ class CreateSalesTable extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('store_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('customer_id')->unsigned();
+            $table->string('code');
+            $table->string('quantity');
+            $table->string('unit_price');
+            $table->string('discount');
+            $table->string('discount_unit');
+            $table->string('payable');
+            $table->string('paid');
+            $table->string('due');
             $table->timestamps();
         });
     }
