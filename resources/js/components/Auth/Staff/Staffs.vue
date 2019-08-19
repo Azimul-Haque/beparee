bf0f7f75affa7e6065b6aaf5d3ec0fbfadadbb73
orbachinujbuk <orbachinujbuk@gmail.com>
2019-08-19 06:16:48 +0600

    Update!

--- resources/js/components/Auth/Staff/Staffs.vue
+++ resources/js/components/Auth/Staff/Staffs.vue
@@ -4,7 +4,7 @@
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
-                <h1 class="m-0 text-dark">কর্মচারী তালিকা</h1>
+                <h1 class="m-0 text-dark">কর্মচারী তালিকা TesT</h1>
               </div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
