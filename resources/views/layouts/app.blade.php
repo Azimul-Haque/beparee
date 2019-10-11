69a2a2b088bbf5f025edfc7620d2e78ab8e1f55c
orbachinujbuk <orbachinujbuk@gmail.com>
2019-10-11 06:13:55 +0600

    asd asdasd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,17 +87,17 @@
       gtag('config', 'UA-149690696-1');
     </script>
     <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    {{-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PBXL2GW');</script>
+    })(window,document,'script','dataLayer','GTM-PBXL2GW');</script> --}}
     <!-- End Google Tag Manager -->
 </head>
 <body class="stretched">
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBXL2GW"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    {{-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBXL2GW"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> --}}
     <!-- End Google Tag Manager (noscript) -->
     <div id="app">
         <!-- Document Wrapper
