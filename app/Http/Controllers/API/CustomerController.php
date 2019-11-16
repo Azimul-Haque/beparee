671a22578485357ed6e79661f5a3aa73d65926c0
orbachinujbuk <orbachinujbuk@gmail.com>
2019-11-16 11:34:07 +0600

    ASD34

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -76,7 +76,7 @@ class CustomerController extends Controller
         $customer->nid = $request->nid;
         
         $customerdue = Customerdue::where('customer_id', $customer->id)->where('remark', 'পূর্বের বকেয়া')->first();
-        if(!empty($customerdue)) {
+        if(!empty($customerdue) && is_numeric($customerdue->amount)) {
             if($customerdue->amount != $request->ldue) {
                 // aage update, then change
                 $customer->current_due = $customer->current_due - $customerdue->amount + $request->ldue;
